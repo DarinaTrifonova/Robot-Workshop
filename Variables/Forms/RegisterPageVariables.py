f270ee3f8815545003f057245f3fe4eb85f6e7be
TechHuddle <simonadragoeva488@gmail.com>
2020-03-05 10:22:16 +0200

    Simona Homework workshop day 2

--- Variables/Forms/RegisterPageVariables.py
+++ Variables/Forms/RegisterPageVariables.py
@@ -1,1 +1,18 @@
-USER_NAME = "JOHN"
+valid_email = "simona.dragoeva@techhuddle.com"
+user_name = "Simona"
+family_name = "Dragoeva"
+passwd = "Admin123"
+adr = "TestingAddress"
+cty = "Sofia"
+option = "10"
+post_c = "12345"
+mobile = "0258741369"
+alias = "Test_alias"
+invalid_email = "simona.test.abv.bg"
+
+
+
+
+
+
+
